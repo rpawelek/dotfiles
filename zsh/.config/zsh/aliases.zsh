5990982a562de427c2f8c66eade3dc55bed1b03c
Rafał Pawełek <rpawelek@proton.me>
2023-12-29 17:18:37 +0100

    Use zap plugin manager

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,3 +1,23 @@
 #!/bin/sh
-# Empty for now
+alias nv="nvim"
+alias vi="nvim"
+
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+
+alias df="df -h"
+alias free="free -m"
+
+alias paci="sudo pacman -Syu"
+alias pacu="sudo pacman -Syyu"
+alias pacs="sudo pacman -Ss"
+
+alias gs="git status"
+alias gc="git commit -m"
+alias gca="git commit -am"
+alias ga="git add"
+alias gaa="git add ."
+alias gp="git push"
+alias gu="git pull"
 
