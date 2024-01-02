364043c0acc7a379898807c31481d37d5426008b
Rafał Pawełek <rpawelek@proton.me>
2024-01-02 18:34:04 +0100

    Initialize Neovim config

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+local keymap = vim.keymap
+
+-- Directory navigation
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
---
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  config = function()
+    require("nvim-tree").setup({
+      filters = {
+        dotfiles = false,
+      },
+      view = {
+        adaptive_size = true,
+      },
+    })
+  end,
+}
+
