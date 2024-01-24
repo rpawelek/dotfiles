612895b97d29614817db93725a2b83d726c4fc90
Rafał Pawełek <rpawelek@proton.me>
2024-01-24 15:14:22 +0100

    Add Aftman XDG path

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -34,4 +34,5 @@ export _Z_DATA="$XDG_DATA_HOME/z"
 export NVM_DIR="$XDG_DATA_HOME/nvm"
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
+export AFTMAN_ROOT="$XDG_DATA_HOME/aftman"
 
