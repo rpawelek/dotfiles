5f886d03a5b0c32903782b37829071052812fa88
Rafał Pawełek <rpawelek@proton.me>
2024-08-18 21:52:33 +0200

    Kickstart neovim config

---
+++ nvim/.config/nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,10 @@
+return { 
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    name = "kanagawa",
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme("kanagawa")
+    end,
+}
+
---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {"lua", "c", "go", "html", "markdown", "markdown_inline" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
+}
+
