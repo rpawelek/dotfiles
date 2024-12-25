56c21da21141ea287d145827084889f4c487f66f
Rafał Pawełek <rpawelek@proton.me>
2024-12-25 18:02:58 +0100

    A fresh start ;)

--- nvim/.config/nvim/lua/options.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
